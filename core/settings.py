3df19461277d4cc4e68fb041659acade7ec91650
CODED BLACK <codedb693@gmail.com>
2026-02-16 12:48:24 +0300

    Update Django deployment checklist URL

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-dv()kbum_phwlyc91*)@4u1*qntqnp@ji%#5)yfqr!yeb3r30y'
